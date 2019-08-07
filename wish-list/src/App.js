0dd1fa9c0713f42f7992c104f7be847fc3a25411
Phil Smith <pbsmithtbn@gmail.com>
2019-08-07 12:55:35 -0500

    Add instructions for the exercise

--- wish-list/src/App.js
+++ wish-list/src/App.js
@@ -5,6 +5,10 @@ import "./App.css";
 import PersonCard from "./components/PersonCard";
 import ItemList from "./components/ItemList";
 
+//  Now that you've done it with the useEffect hooks, it's time to refactor it with class components and life-cycle methods.  The three most commonly used life-cycle methods are componentDidMount, componentDidUpdate and componentDidUnmount.  You'll only need two of those for this exercise.
+
+// Note about componentDidUpdate:  this method needs to track a value on state or props much like useEffect.  The big difference is that we have to put in the comparison logic ourselves to compare previous-state (prevState) with the current-state.  Remember what you tracked in the dependancy array in the hooks version.
+
 class App extends Component {
   constructor() {
     super();
@@ -15,33 +19,11 @@ class App extends Component {
     };
   }
 
-  componentDidMount() {
-    axios
-      .get("https://powerful-beyond-74276.herokuapp.com/api/wishlist")
-      .then(res => {
-        this.setState({ people: res.data });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+  // Fetch the list of people using the appropriate life-cycle method
+  // Code here!
 
-  componentDidUpdate(prevState) {
-    if (prevState.selectedPerson !== this.state.selectedPerson) {
-      axios
-        .get(
-          `https://powerful-beyond-74276.herokuapp.com/api/wishlist/${
-            this.state.selectedPerson.id
-          }`
-        )
-        .then(res => {
-          this.setState({ items: res.data });
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    }
-  }
+  // Fetch the list of items using the appropriate life-cycle method
+  //Code here!
 
   setSelectedPerson = person => {
     this.setState({ selectedPerson: person });
@@ -53,9 +35,8 @@ class App extends Component {
         {this.state.people.map(person => (
           <div onClick={() => this.setSelectedPerson(person)} key={person.id}>
             <PersonCard person={person} />
-            {person.id === this.state.selectedPerson.id && (
-              <ItemList list={this.state.items} />
-            )}
+            {/* Like before fill in the blank in the line below */}
+            {/* {person.id === __blank__ && <ItemList list={this.state.items} />} */}
           </div>
         ))}
       </div>
